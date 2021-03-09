2d34a9640f6f09330ea5473b0f60018a2f795d78
Lucas Neves <lucas.neves@codeblox.com.br>
2021-03-08 23:53:54 -0300

    Body parser

--- app.js
+++ app.js
@@ -2,9 +2,21 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.urlencoded({ extended: true}));
+
 app.use('/novo', (req, res, next) => {
   console.log('add job')
   res.send('<p>Adicionar Job</p>')
+})
+
+app.use('/add-product', (req, res, next) => {
+  console.log('add job')
+  res.send('<form action="/product" method="POST"><input type="text" name="title" /><button type="submit">Add Product</button></form>')
+})
+
+app.post('/product', (req, res, next) => {
+  console.log(req.body);
+  res.redirect('/');
 })
 
 app.use('/', (req, res, next) => {
