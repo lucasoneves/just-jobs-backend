4abdcc40e6ce01af5ce6f452fb30d6b6024a3376
lucasoneves <casluhc@gmail.com>
2021-04-09 18:35:09 -0300

    static files

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
-const express = require('express');
 const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const adminRoute = require('./routes/admin');
-const shopRoute = require('./routes/index');
 const app = express();
-
 app.use(express.static(path.join(__dirname, 'assets')));
 
-app.use('/admin', adminRoute);
-app.use(shopRoute);
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminData.routes);
+app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
-})
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
 
-// app.use(express.static(path.join(__dirname, 'assets')));
-
-
-app.listen(3000)
+app.listen(3000);
