438e0dbe62545a9cc4f2e6703997551a615f3de8
lucasoneves <casluhc@gmail.com>
2021-05-03 16:07:31 -0300

    Added a layout page

--- routes/shop.js
+++ routes/shop.js
@@ -8,9 +8,8 @@ const adminData = require('./admin');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  console.log('shop.js', adminData.products);
   const products = adminData.products;
-  res.render('shop', {prods: products, docTitle: 'Shop'});
+  res.render('shop', {prods: products, pageTitle: 'Shop', path: '/'});
 });
 
 module.exports = router;
